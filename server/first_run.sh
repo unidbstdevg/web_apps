5f01441049f94b6f129c66cc91483fd75dae59a2
Evgeny <sev.20@uni-dubna.ru>
2022-02-24 22:23:08 +0300

    [server] Forward mysql port to use on host machine

--- server/first_run.sh
+++ server/first_run.sh
@@ -3,6 +3,7 @@ docker build . -t my_lamp
 docker run --name uni_wa_serv \
     --detach \
     -p "80:80" \
+    -p "3306:3306" \
     -v ${PWD}/../app/:/app \
     -v ${PWD}/mysql:/var/lib/mysql \
     my_lamp
