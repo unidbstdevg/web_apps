38435a3263d8e3f04effb201e8da892255b12351
Evgeny <sev.20@uni-dubna.ru>
2022-05-05 14:07:33 +0300

    [django] Rename model TasksList to Task

--- django/myapp/models.py
+++ django/myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class TasksList(models.Model):
+class Task(models.Model):
     title = models.CharField(max_length=50)
     desc = models.CharField(max_length=50)
 
--- django/myapp/views.py
+++ django/myapp/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import TasksList
+from .models import Task
 
 
 def hello(req):
@@ -11,7 +11,7 @@ def hello(req):
     res += "<th>desc</th>"
     res += "</tr>"
 
-    for x in TasksList.objects.all():
+    for x in Task.objects.all():
         new_tr = "<tr>"
         # new_tr += "title: {}, desc: {}<br>".format(x.title, x.desc)
         new_tr += "<td>{}</td>".format(x.title)
